70852307f179a465278cd41df66893a29184772f
Dussuet Inès <i.dussue@gmail.com>
2018-05-22 09:36:55 +0200

    mise a jour

--- src/reducteurs/donnees.js
+++ src/reducteurs/donnees.js
@@ -5,6 +5,11 @@ const selectList = state => state.liste
 //Actions
 const SET_SIGLES = 'SET_SIGLES'
 
+var elasticsearch = require('elasticsearch')
+  var client = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace'
+  });
 
 //Action creator
 export function setSigles(sigle) {
@@ -15,11 +20,23 @@ export function setSigles(sigle) {
 		}
 }
 
+export function setRech(sigle_nom) {
+	return {
+		type: 'SET_RECH',
+		nom: sigle_nom
+	}
+}
+
 //Reducer
 const donnees = (state =donnees_sigles, action) => {
 	switch(action.type){
 		case 'SET_SIGLES':
+			{client.search({ index: 'sigles', body: { query: { match_all: {}} }}).then(function (resp){ var hits = resp.hits.hits; console.log(hits)}, function (err) { console.trace(err.message)})
+			//return state //=> ici ajout du sigle dans elasticsearch
 			return ({liste :[...selectList(state), {nom: action.nom, def: action.def}]})
+		}
+		case 'SET_RECH':
+			return state  /*requete pour obtenir le resultat de la recherche */
 		default:
 			return state
 	}
