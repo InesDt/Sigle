995b6aff5f77b9dc4ddf2d4c46b7a639225bf3d2
Dussuet Inès <i.dussue@gmail.com>
2018-05-29 14:04:04 +0200

    mise a jour

--- src/composants/BoutonSuppr.js
+++ src/composants/BoutonSuppr.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import {fetch_remSigle} from '../reducteurs/donnees'
 import { selectSigle, selectDef } from '../reducteurs/form'
 
-const Presentation = props => ( <button onClick={() =>  props.onClick(props.value, props.sigle_rech)} > -_- </button>)
+const Presentation = props => ( <button onClick={() =>  props.onClick(props.value, props.sigle_rech)} > x__x </button>)
 
 const mapStateToProps = state => ({
   sigle_rech: selectSigle(state)
